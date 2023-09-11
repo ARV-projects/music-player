6d9e097b14cc36ebd3a22b7deaa7a3131fb4f808
ARV-projects <alexander.raducan@gmail.com>
2023-09-11 22:48:48 +0300

    finished progress bar

--- script.js
+++ script.js
@@ -116,12 +116,22 @@ function updateProgressBar(e) {
         let currentSeconds = Math.floor(currentTime % 60);
         if (currentSeconds < 10) {
             currentSeconds = `0${currentSeconds}`;
-            currentTimeEl.textContent = `${currentMinutes}:${currentSeconds}`;
         }
+        currentTimeEl.textContent = `${currentMinutes}:${currentSeconds}`;
     }
+}
+
+// Set Progress Bar
+function setProgressBar(e) {
+    const width = this.clientWidth;
+    const clickX = e.offsetX;
+    const {duration} = music;
+    music.currentTime = (clickX / width) * duration;
 }
 
 // Event Listeners
 prevBtn.addEventListener('click', prevSong);
 nextBtn.addEventListener('click', nextSong);
+music.addEventListener('ended', nextSong);
 music.addEventListener('timeupdate', updateProgressBar);
+progressContainer.addEventListener('click', setProgressBar);
