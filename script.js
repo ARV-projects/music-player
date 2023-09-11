ceb7a28c122f74042bc172eef6d6f70a4e8f2c7d
ARV-projects <alexander.raducan@gmail.com>
2023-09-11 20:27:37 +0300

    new songs

--- script.js
+++ script.js
@@ -1,7 +1,34 @@
+const image = document.querySelector('img');
+const title = document.getElementById('title');
+const artist = document.getElementById('artist');
 const music = document.querySelector('audio');
 const prevBtn = document.getElementById('prev');
 const playBtn = document.getElementById('play');
 const nextBtn = document.getElementById('next');
+
+// Music
+const songs = [
+   {
+    name: 'jacinto-1',
+    displayName: 'Electric Chill Machine',
+    artist: 'Jacinto Design',
+   }, 
+   {
+    name: 'jacinto-2',
+    displayName: 'Seven Nation Army (Remix)',
+    artist: 'Jacinto Design',
+   },
+   {
+    name: 'jacinto-3',
+    displayName: 'Goodnight, Disco Queen',
+    artist: 'Jacinto Design',
+   },
+   {
+    name: 'metric-1',
+    displayName: 'Front Row (Remix)',
+    artist: 'Metric/Jacinto Design',
+   },
+];
 
 //Check if Playing
 let isPlaying = false;
@@ -26,3 +53,14 @@ function pauseSong() {
 playBtn.addEventListener('click', function() {
     (isPlaying ? pauseSong() : playSong());
 });
+
+// Update the DOM
+function loadSong(song) {
+    title.textContent = song.displayName;
+    artist.textContent = song.artist;
+    music.src = `music/${song.name}.mp3`;
+    image.src = `img/${song.name}.jpg`;
+}
+
+// On Load - Select First Song
+loadSong(songs[3]);
